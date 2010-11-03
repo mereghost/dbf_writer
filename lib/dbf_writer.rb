89df67265aa3a3f0bdf367b1ec54ce2a1894bb28
Marcello <marcello.rocha@gmail.com>
2010-11-03 13:18:20 -0200

    Starting the binary writing with empty files. After the descriptors are right, I'll jump to the row data.
    Signed-off-by: Marcello <marcello.rocha@gmail.com>

--- lib/dbf_writer.rb
+++ lib/dbf_writer.rb
@@ -7,8 +7,49 @@ class DbfWriter
   end
 
   def add_field(field)
-    @fields << field
+    @fields << {:name => field, :total_length => 50}
   end
 
+  def to_binary_string
+    header
+#    eof
+    @data
+  end
+
+  private
+  def column_definitions
+    arr = @fields.inject([]) do |stuff, hash|
+      columns = []
+      columns << hash[:name].upcase
+      columns << "C"
+      columns << 0 #Reserved
+      columns << hash[:total_length]
+      columns << 0 #WorkAreaId
+      columns << 0 #multiuser
+      columns << 0 #Setfield
+      columns << 0 #reserved
+      columns << 0 #reserved
+      columns << 0 #IncludeMdx
+      stuff << columns.pack('a10ax4CCx15')
+    end
+    @data << arr.flatten.join('')
+  end
+
+  def header
+    header = [3]
+    header << 10
+    header << 11
+    header << 3
+    header << 0
+    header << (@fields.size * 32 + 33)
+    header << @fields.inject(0) { |s,f| s + f[:total_length]} + 1
+    @data = header.pack('CCCCVvvxxxxxxxxxxxxxxxxxxxx')
+    column_definitions
+    @data << "\x0D"
+  end
+
+  def eof
+    @data << "\x1A"
+  end
 end
 
--- spec/dbf_writer_spec.rb
+++ spec/dbf_writer_spec.rb
@@ -6,7 +6,13 @@ describe "DbfWriter" do
     dbf = DbfWriter.new
     dbf.add_field('DATA')
     dbf.fields.size.should == 1
-    dbf.fields.should == ['DATA']
+    dbf.fields.should == [{:name => 'DATA', :total_length => 50}]
+  end
+
+  it "should write a valid empty file" do
+    dbf = DbfWriter.new
+    dbf.add_field 'data'
+    dbf.to_binary_string.should == File.open('spec/support/sample_dbf/single_column.dbf','rb').readlines.join('')
   end
 
 #  it "should write a single character column DBF" do
